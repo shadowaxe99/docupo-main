85cdc70c6ba212b060cf235f05b77be58d1e30f1
username <128367359+shadowaxe99@users.noreply.github.com>
2023-08-02 22:17:54 -0400

    Update for production

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def reviewRepoStructure():
         return jsonify({'message': 'Invalid repository structure'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
